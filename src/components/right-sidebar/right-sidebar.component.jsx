410ba73d0d91759e330befaf044b6ec1dcfb33ed
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-21 19:14:51 -0400

    Created right side bar content

--- src/components/right-sidebar/right-sidebar.component.jsx
+++ src/components/right-sidebar/right-sidebar.component.jsx
@@ -1,7 +1,47 @@
-import { Container } from "./right-sidebar.styles";
+import {
+  Container,
+  FollowCard,
+  Title,
+  FollowLinks,
+  Avatar,
+  Recommendations,
+} from "./right-sidebar.styles";
 
 const RightSidebar = () => {
-  return <Container>Right Side</Container>;
+  return (
+    <Container>
+      <FollowCard>
+        <Title>
+          Add To Your Feed
+          <img src="/images/feed-icon.svg" alt="Feed" />
+        </Title>
+        <FollowLinks>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FollowLinks>
+        <Recommendations>
+          <span>View All Recommendations</span>
+          <img src="/images/right-icon.svg" alt="Right" />
+        </Recommendations>
+      </FollowCard>
+    </Container>
+  );
 };
 
 export default RightSidebar;
--- src/components/right-sidebar/right-sidebar.styles.js
+++ src/components/right-sidebar/right-sidebar.styles.js
@@ -2,4 +2,74 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   grid-area: rightside;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+`;
+
+export const FollowLinks = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin-right: auto;
+
+      button {
+        border: 2px solid rgb(0, 0, 0, 0.6);
+        border-radius: 15px;
+        padding: 5px 15px;
+        color: rgb(0, 0, 0, 0.6);
+      }
+    }
+  }
+`;
+
+export const Avatar = styled.div`
+  background-image: url("https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 48px;
+  height: 48px;
+  margin-right: 10px;
+`;
+
+export const Recommendations = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  color: #0a66c2;
+  margin-top: 5px;
+  font-weight: 500;
+  font-size: 16px;
+
+  img {
+    margin-left: 10px;
+  }
 `;
