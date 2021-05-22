c6660e9570b641977c7fc7c79ca144a263f0740e
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-21 20:09:44 -0400

    Created the add post box and added in the necessary icons to display underneath add post button

--- src/components/main-content/main-content.component.jsx
+++ src/components/main-content/main-content.component.jsx
@@ -1,7 +1,44 @@
-import { Container } from "./main-content.styles";
+import {
+  CommonCard,
+  Container,
+  Photo,
+  PostButton,
+  IconContainer,
+  PostContainer,
+  PostInfo,
+} from "./main-content.styles";
 
 const MainContent = () => {
-  return <Container>Main</Container>;
+  return (
+    <Container>
+      <CommonCard>
+        <PostContainer>
+          <PostInfo>
+            <Photo />
+            <PostButton>Start A Post</PostButton>
+          </PostInfo>
+          <IconContainer>
+            <button>
+              <img src="/images/photo-icon.svg" alt="Photo" />
+              Photo
+            </button>
+            <button>
+              <img src="/images/video-icon.svg" alt="Photo" />
+              Video
+            </button>
+            <button>
+              <img src="/images/event-icon.svg" alt="Photo" />
+              Event
+            </button>
+            <button>
+              <img src="/images/article-icon.svg" alt="Photo" />
+              Write Article
+            </button>
+          </IconContainer>
+        </PostContainer>
+      </CommonCard>
+    </Container>
+  );
 };
 
 export default MainContent;
--- src/components/main-content/main-content.styles.js
+++ src/components/main-content/main-content.styles.js
@@ -2,4 +2,79 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   grid-area: main;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+export const PostInfo = styled.div`
+  display: flex;
+  align-items: center;
+  color: #958b7b;
+`;
+
+export const PostButton = styled.button`
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  flex: 1;
+  height: 40px;
+  border-radius: 20px;
+  margin-left: 10px;
+  padding-left: 10px;
+  color: rgba(0, 0, 0, 0.6);
+  text-align: left;
+  cursor: pointer;
+
+  :hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
+
+export const Photo = styled.div`
+  background-image: url("/images/user.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 10px 5px;
+
+  button {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    border-radius: 5px;
+    color: rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.08);
+    }
+
+    img {
+      margin-right: 5px;
+    }
+  }
 `;
