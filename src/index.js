56f03d836163b40856454d074605cbe80b1de22f
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-24 19:31:59 -0400

    Added initial redux setup with sotre and userReducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import store from "./redux-state/store";
+
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
